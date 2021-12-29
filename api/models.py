588120d66c9695e307bf063704802398119773b5
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-12-29 14:19:24 +0900

    Add scale column

--- api/models.py
+++ api/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Companies(models.Model):
     ko_name = models.CharField(max_length=64)
     en_name = models.CharField(max_length=64)
-    amount = models.IntegerField()
+    amount = models.IntegerField(null=True)
     category = models.CharField(max_length=32, null=True)
     industry = models.CharField(max_length=16, null=True)
-    logo = models.CharField(max_length=256)
+    scale = models.CharField(max_length=16, null=True)
+    logo = models.CharField(max_length=256, null=True)
