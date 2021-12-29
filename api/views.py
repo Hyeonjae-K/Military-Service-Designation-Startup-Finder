958fd8738d2592ac32f3e29f51b4e0f38bbf0a00
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-12-29 16:56:29 +0900

    Add index view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Companies
+
+
+def index(request):
+    page = request.GET.get('page', '1')
+    companies = Companies.objects.order_by('-amount')
+    paginator = Paginator(companies, 15)
+    page_obj = paginator.get_page(page)
+    context = {'companies': page_obj, 'page': page}
+    return render(request, 'index.html', context=context)
