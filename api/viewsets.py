84f1419869b780d2ff16724b715da9968ea02f39
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-12-29 15:42:30 +0900

    Add cron job

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,7 +1,13 @@
 from rest_framework import viewsets
+from apscheduler.schedulers.background import BackgroundScheduler
 
 from .models import Companies
 from .serializers import CompaniesSerializer
+from .crawling import update_data
+
+sched = BackgroundScheduler()
+sched.add_job(update_data, 'cron', hour=12)
+sched.start()
 
 
 class CompaniesViewSet(viewsets.ModelViewSet):
