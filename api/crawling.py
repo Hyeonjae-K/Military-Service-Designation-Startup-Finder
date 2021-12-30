26bb3df67290b8408e43421a8e5fe41ec5029d8e
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-12-30 15:26:37 +0900

    Fix duplication problem

--- api/crawling.py
+++ api/crawling.py
@@ -1,26 +1,26 @@
 import django
 django.setup()
 
-from .models import Companies
-
-import requests
-import pandas as pd
-from datetime import date
-from urllib import request
 from django.utils import timezone
+from urllib import request
+from datetime import date
+import pandas as pd
+import requests
+
+from .models import Companies
 
 
 today = str(date.today())
 xls_path = 'data_files/%s.xls' % today
-startups = []
+startups = {}
 company_names = company_industries = company_scales = None
 
 
 def _get_data():
-    global company_names, company_industries, company_scales
+    global startups, company_names, company_industries, company_scales
 
-    request.urlretrieve(
-        'https://work.mma.go.kr/caisBYIS/search/downloadBYJJEopCheExcel.do', xls_path)
+    # request.urlretrieve(
+    #     'https://work.mma.go.kr/caisBYIS/search/downloadBYJJEopCheExcel.do', xls_path)
 
     df = pd.read_excel(xls_path)[['업체명', '업종', '기업규모']]
     del_strs = ['(주)', '(유)', '(합)', '㈜', '주식회사']
@@ -35,18 +35,17 @@ def _get_data():
     amounts = requests.get(
         'https://startupspace.kr/api/amounts').json()['item']
     for item in items:
-        startup = {
+        key = item['id']
+        startups[key] = {
             'ko_name': item['name'],
-            'en_name': item['id'],
             'category': item['category']
         }
-        if startup['en_name'] in amounts:
-            startup['amount'] = amounts[startup['en_name']]
+        if key in amounts:
+            startups[key]['amount'] = amounts[key]
         try:
-            startup['logo'] = item['logo']['url']
+            startups[key]['logo'] = item['logo']['url']
         except:
-            startup['logo'] = None
-        startups.append(startup)
+            startups[key]['logo'] = None
 
 
 def update_data():
@@ -54,12 +53,12 @@ def update_data():
     companies, new_companies, update_companies = [], [], []
     datetime = timezone.now()
 
-    for startup in startups:
+    for key, startup in startups.items():
         try:
             idx = company_names.index(startup['ko_name'])
             companies.append({
+                'en_name': key,
                 'ko_name': startup['ko_name'],
-                'en_name': startup['en_name'],
                 'amount': startup['amount'],
                 'category': startup['category'],
                 'industry': company_industries[idx],
@@ -71,14 +70,14 @@ def update_data():
 
     for data in companies:
         try:
-            company = Companies.objects.get(ko_name=data['ko_name'])
+            company = Companies.objects.get(en_name=data['en_name'])
             company.update_date = datetime
             update_companies.append(company)
         except:
             company = Companies(ko_name=data['ko_name'], en_name=data['en_name'], amount=data['amount'], category=data['category'],
                                 industry=data['industry'], scale=data['scale'], logo=data['logo'], update_date=datetime)
             new_companies.append(company)
-    
+
     if update_companies:
         Companies.objects.bulk_update(update_companies, ['update_date'])
     if new_companies:
