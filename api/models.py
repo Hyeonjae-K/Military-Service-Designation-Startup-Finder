4d4766fbb2fc58aba30b36ad0d620dd8f3c1fea0
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2022-01-06 13:42:46 +0900

    ADD - Recruitments model 추가

--- api/models.py
+++ api/models.py
@@ -10,3 +10,12 @@ class Startups(models.Model):
     scale = models.CharField(max_length=16)
     logo = models.CharField(max_length=256, null=True)
     update_date = models.DateTimeField()
+
+
+class Recruitments(models.Model):
+    name = models.CharField(max_length=64, primary_key=True)
+    indystry = models.CharField(max_length=32)
+    response_rate = models.FloatField()
+    response_day = models.IntegerField()
+    logo = models.CharField(max_length=256)
+    update_date = models.DateField()
